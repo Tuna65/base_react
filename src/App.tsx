88018861861372786de1a14f0f89150bcb6ab655
Tuna65 <patuan657@gmail.com>
2025-07-28 10:10:18 +0700

    feat: rebasse

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConfigProvider } from "antd";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -10,31 +9,28 @@ import LoginLayout from "./pages/auth";
 import { configAntdProvider } from "./utils";
 import { PATHNAME } from "./utils/Pathname";
 
-const queryClient = new QueryClient();
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <ConfigProvider theme={configAntdProvider}>
-          <Routes>
-            <Route path="/" element={<Navigate replace to={PATHNAME.AUTH.LOGIN} />} />
-            <Route element={<LoginLayout />}>
-              <Route path={PATHNAME.AUTH.LOGIN} element={<Login />} />
-              <Route path={PATHNAME.AUTH.REGISTER} element={<Register />} />
+      <ConfigProvider theme={configAntdProvider}>
+        <Routes>
+          <Route path="/" element={<Navigate replace to={PATHNAME.AUTH.LOGIN} />} />
+          <Route element={<LoginLayout />}>
+            <Route path={PATHNAME.AUTH.LOGIN} element={<Login />} />
+            <Route path={PATHNAME.AUTH.REGISTER} element={<Register />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route element={<Layout />}>
+              {routerList.map((r, index) => {
+                return <Route path={r.path} element={r.component} key={`router-${index}`} />;
+              })}
+              <Route path={PATHNAME.COMMING_SOON} element={<ComingSoon />} />
+              <Route path="*" element={<Navigate replace to={PATHNAME.COMMING_SOON} />} />
             </Route>
-            
-            <Route element={<ProtectedRoute />}>
-              <Route element={<Layout />}>
-                {routerList.map((r, index) => {
-                  return <Route path={r.path} element={r.component} key={`router-${index}`} />;
-                })}
-                <Route path={PATHNAME.COMMING_SOON} element={<ComingSoon />} />
-                <Route path="*" element={<Navigate replace to={PATHNAME.COMMING_SOON} />} />
-              </Route>
-            </Route>
-          </Routes>
-        </ConfigProvider>
-      </QueryClientProvider>
+          </Route>
+        </Routes>
+      </ConfigProvider>
     </>
   );
 }
--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,39 +1,19 @@
-import { accountApi } from "@/apis/account";
-import ContainerTablePage from "@/components/ContainerTablePage";
-import { useSearchQuery } from "@/hooks/useQuery";
 import { useTitle } from "@/hooks/useTitle";
-import { PATHNAME } from "@/utils/Pathname";
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
-import useUserService from "./useAccountService";
-import { useSelector } from "react-redux";
-import { keySelector } from "@/store/modules/tanstackKey/selector";
 
 const Users = () => {
   const { t } = useTranslation();
   useTitle(t("Quản lý nhân viên"));
-  const navigate = useNavigate();
-  const { params, onParams } = useSearchQuery();
-  const { columns } = useUserService();
-  const key = useSelector(keySelector);
-
-  const { isPending, error, data, isFetching, isLoading } = useQuery({
-    queryKey: [key.account, params],
-    queryFn: accountApi.find,
-    staleTime: 600000,
-    enabled: !!Object.values(params)?.length,
-    retry: 1,
-  });
 
   return (
-    <ContainerTablePage
-      data={data as any}
-      column={columns as any}
-      loading={isLoading}
-      actionCreate={() => navigate(PATHNAME.USER.CREATE)}
-    />
+    // <ContainerTablePage
+    //   data={}
+    //   column={columns as any}
+    //   loading={false}
+    //   actionCreate={() => navigate(PATHNAME.USER.CREATE)}
+    // />
+    <div className=""></div>
   );
 };
 
